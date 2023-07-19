94afd11aba4a7073be30460441539ab71ccc7650
brunomatosdev <sotamenterprise@gmail.com>
2023-07-19 04:07:29 +0100

    add style

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,47 +28,61 @@ export default function HomePage() {
   return (
     <div>
       <button className={styles.button} onClick={() => setButtonClicked(true)}>
-        Buscar Pools
+        Scan Pools
       </button>
-      <h1 className={styles.title}>PancakeSwap Pools:</h1>
-      {pancakeSwapPools.map((pool, index) => (
-        <div className={styles.pool} key={`pancake-pool-${index}`}>
-          <h2 className={styles.poolTitle}>PANCAKESWAP POOL</h2>
-          <p className={styles.poolInfo}>Token 0 ID: {pool.token0Id}</p>
-          <p className={styles.poolInfo}>Token 0 Symbol: {pool.token0Symbol}</p>
-          <p className={styles.poolInfo}>Token 0 Name: {pool.token0Name}</p>
-          <p className={styles.poolInfo}>
-            Token 0 Price USD: {pool.token0DerivedUSD}
-          </p>
-          <p className={styles.poolInfo}>Token 1 ID: {pool.token1Id}</p>
-          <p className={styles.poolInfo}>Token 1 Symbol: {pool.token1Symbol}</p>
-          <p className={styles.poolInfo}>Token 1 Name: {pool.token1Name}</p>
-          <p className={styles.poolInfo}>
-            Token 1 Price USD: {pool.token1DerivedUSD}
-          </p>
-          <br />
+      <div className={styles.container}>
+        <h1 className={styles.title}>PancakeSwap Pools:</h1>
+        <div className={styles.scrollContainer}>
+          {pancakeSwapPools.map((pool, index) => (
+            <div className={styles.pool} key={`pancake-pool-${index}`}>
+              <h2 className={styles.poolTitle}>PANCAKESWAP POOL</h2>
+              <p className={styles.poolInfo}>Token 0 ID: {pool.token0Id}</p>
+              <p className={styles.poolInfo}>
+                Token 0 Symbol: {pool.token0Symbol}
+              </p>
+              <p className={styles.poolInfo}>Token 0 Name: {pool.token0Name}</p>
+              <p className={styles.poolInfo}>
+                Token 0 Price USD: {pool.token0DerivedUSD}
+              </p>
+              <p className={styles.poolInfo}>Token 1 ID: {pool.token1Id}</p>
+              <p className={styles.poolInfo}>
+                Token 1 Symbol: {pool.token1Symbol}
+              </p>
+              <p className={styles.poolInfo}>Token 1 Name: {pool.token1Name}</p>
+              <p className={styles.poolInfo}>
+                Token 1 Price USD: {pool.token1DerivedUSD}
+              </p>
+              <br />
+            </div>
+          ))}
         </div>
-      ))}
 
-      <h1 className={styles.title}>Uniswap V3 Pools:</h1>
-      {uniswapPools.map((pool, index) => (
-        <div className={styles.pool} key={`uniswap-pool-${index}`}>
-          <h2 className={styles.poolTitle}>UNISWAP POOL</h2>
-          <p className={styles.poolInfo}>Token 0 ID: {pool.token0Id}</p>
-          <p className={styles.poolInfo}>Token 0 Symbol: {pool.token0Symbol}</p>
-          <p className={styles.poolInfo}>Token 0 Name: {pool.token0Name}</p>
-          <p className={styles.poolInfo}>
-            Token 0 Price ETH: {pool.token0DerivedETH}
-          </p>
-          <p className={styles.poolInfo}>Token 1 ID: {pool.token1Id}</p>
-          <p className={styles.poolInfo}>Token 1 Symbol: {pool.token1Symbol}</p>
-          <p className={styles.poolInfo}>Token 1 Name: {pool.token1Name}</p>
-          <p className={styles.poolInfo}>
-            Token 1 Price ETH: {pool.token1DerivedETH}
-          </p>
-          <br />
+        <h1 className={styles.title}>Uniswap V3 Pools:</h1>
+        <div className={styles.scrollContainer}>
+          {uniswapPools.map((pool, index) => (
+            <div className={styles.pool} key={`uniswap-pool-${index}`}>
+              <h2 className={styles.poolTitle}>UNISWAP POOL</h2>
+              <p className={styles.poolInfo}>Token 0 ID: {pool.token0Id}</p>
+              <p className={styles.poolInfo}>
+                Token 0 Symbol: {pool.token0Symbol}
+              </p>
+              <p className={styles.poolInfo}>Token 0 Name: {pool.token0Name}</p>
+              <p className={styles.poolInfo}>
+                Token 0 Price ETH: {pool.token0DerivedETH}
+              </p>
+              <p className={styles.poolInfo}>Token 1 ID: {pool.token1Id}</p>
+              <p className={styles.poolInfo}>
+                Token 1 Symbol: {pool.token1Symbol}
+              </p>
+              <p className={styles.poolInfo}>Token 1 Name: {pool.token1Name}</p>
+              <p className={styles.poolInfo}>
+                Token 1 Price ETH: {pool.token1DerivedETH}
+              </p>
+              <br />
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
 
       <p className={styles.totalPools}>
         Total PancakeSwap V3 pools listed: {pancakeSwapPools.length}
